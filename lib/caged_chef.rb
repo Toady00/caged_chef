e80f2386cecf84ac5a7900f24217e3c0fb74c530
Brandon Dennis <toady00@gmail.com>
2013-05-18 17:15:30 -0400

    Fixes naming error.
    Faraday was trying to load CagedChef instead of ChefAuth.

--- lib/caged_chef.rb
+++ lib/caged_chef.rb
@@ -2,9 +2,9 @@ require 'faraday'
 require 'caged_chef/version'
 
 module CagedChef
-  autoload :CagedChef, 'caged_chef/chef_auth'
+  autoload :ChefAuth, 'caged_chef/chef_auth'
 
   if Faraday.respond_to? :register_middleware
-    Faraday.register_middleware :request, :caged_chef => lambda { CagedChef }
+    Faraday.register_middleware :request, :caged_chef => lambda { ChefAuth }
   end
 end
